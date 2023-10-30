09a29ece7a4f38bbb857ccbc6258e21f3a5a25ee
naz882 <guliyev.n.d@gmail.com>
2023-10-30 07:55:50 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nazym Guliyev on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
